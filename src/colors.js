64060723410096c4b73fdbcfbc3060be41eedb5d
Chris Stroud <chris.stroud@thebeansgroup.com>
2019-07-13 11:21:06 +0100

    Add more colours to palette

--- src/colors.js
+++ src/colors.js
@@ -2,5 +2,9 @@ const Colors = new Map()
 Colors.set('font', '#2e3333')
 Colors.set('font.muted', '#828585')
 Colors.set('muted', '#828585')
+Colors.set('link', '#00b8a9')
+Colors.set('blue', '#007e8a')
+Colors.set('green', '#77bf2a')
+Colors.set('oranage', '#ff8100')
 
 export default Colors
